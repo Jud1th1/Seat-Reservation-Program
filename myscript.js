b79d8db9b5cf56372703c2631b4b32c16b6f01bf
Jud1th1 <127470572+Jud1th1@users.noreply.github.com>
2026-02-23 22:10:50 -0500

    Adding seats right side and left

--- myscript.js
+++ myscript.js
@@ -5,13 +5,33 @@ let counter = 1;
 
 rows.forEach (function (row){
     //loop through each row 
-    html += `<div class="label>${row}</div>`;
+    html += `<div class="label">${row}</div>`;
 
     for(let i=0; i < 3; i++){
         //nested loop to generate seats for each row
         html += `<div id="${row + counter}">${counter}</div>`;
         counter++;
     }
-
-    document.getElementById('left').innerHTML = html;
+    counter = counter + 12;
+    
 });
+
+document.getElementById('left').innerHTML = html;
+
+
+
+//Right side aka reverse -> Seats first then label.
+html ="";
+counter = 1;
+
+rows.forEach (function (row){
+    counter = counter + 12; //add 12 seats first THEN do counter
+    for(let i=0; i < 3; i++){
+        html += `<div id="${row + counter}">${counter}</div>`;
+        counter++;
+    }
+    //Add label at the end
+    html += `<div class="label">${row}</div>`;
+});
+
+document.getElementById('right').innerHTML = html;
